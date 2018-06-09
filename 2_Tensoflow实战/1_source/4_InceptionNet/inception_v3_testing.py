8144d535f496a6a9fa5bdc7d0e6816454defcced
GhaoTong <gt333@126.com>
2018-06-09 09:57:07 +0800

    enter

--- 2_Tensoflow实战/1_source/4_InceptionNet/inception_v3_testing.py
+++ 2_Tensoflow实战/1_source/4_InceptionNet/inception_v3_testing.py
@@ -1,11 +1,68 @@
 import tensorflow as tf
-from inception_tensorflow import *
+import inception_tensorflow.inception_v3
+import inception_tensorflow.inception_utils
+import time
+from datetime import datetime
+import math
+import argparse
+from six.moves import xrange  # pylint: disable=redefined-builtin
+import sys
 trunc_normal = lambda stddev : tf.truncated_normal_initializer(0.0, stddev)
 
+FLAGS = None
 
 
 
+def time_tensorflow_run(session, target, info_string):
+    num_steps_burn_in = 10
+    total_duration = 0.0
+    total_duration_squared = 0.0
+    for i in xrange(FLAGS.num_batches + num_steps_burn_in):
+        start_time = time.time()
+        _ = session.run(target)
+        duration = time.time() - start_time
+        if i >= num_steps_burn_in:
+            if not i % 10:
+                print ('%s: step %d, duration = %.3f' %
+                    (datetime.now(), i - num_steps_burn_in, duration))
+            total_duration += duration
+            total_duration_squared += duration * duration
+    mn = total_duration / FLAGS.num_batches
+    vr = total_duration_squared / FLAGS.num_batches - mn * mn
+    sd = math.sqrt(vr)
+    print ('%s: %s across %d steps, %.3f +/- %.3f sec / batch' %
+            (datetime.now(), info_string, FLAGS.num_batches, mn, sd))
 
+
+def main (_):
+    height, width = 299, 299
+    inputs = tf.random_uniform((FLAGS.batch_size, height, width, 3))
+    with inception_tensorflow.inception_v3.slim.arg_scope(inception_tensorflow.inception_utils.inception_arg_scope()):
+        logits, end_point = inception_tensorflow.inception_v3.inception_v3(inputs,is_training= False)
+
+
+    init = tf.global_variables_initializer()
+    sess = tf.Session()
+    sess.run(init)
+    time_tensorflow_run(sess, logits, "Forward")
+    
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser()
+    parser.add_argument(
+        '--batch_size',
+        type=int,
+        default=32,
+        help='Batch size.'
+    )
+    parser.add_argument(
+        '--num_batches',
+        type=int,
+        default=100,
+        help='Number of batches to run.'
+    )
+    FLAGS, unparsed = parser.parse_known_args()
+    print (FLAGS, unparsed)
+    tf.app.run(main=main, argv=[sys.argv[0]] + unparsed)
 
 
 
