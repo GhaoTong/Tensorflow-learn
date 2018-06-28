9eff8153491eea0aaf64719799a1b227568aa995
GhaoTong <gt333@126.com>
2018-06-28 17:46:02 +0800

    enter

--- 2_Tensoflow实战/1_source/7_LSTM/ptb/lstm.py
+++ 2_Tensoflow实战/1_source/7_LSTM/ptb/lstm.py
@@ -31,49 +31,48 @@ class PTBModel(object):
         num_steps = input_.num_steps
         size = config.hidden_size
         vocab_size = config.vocab_size
-    def lstm_cell():
-            return tf.contrib.rnn.BasicLSTMCell(size, fotget_biase=0.0, state_is_tuple=True)
-        attn_cell = lstm_cell
-        if is_training and config.keep <1:
-            def attn_cell():
-                return tf.contrib.rnn.DropoutWrapper(lstm_cell(),output_keep_prob=config.keep_prob)
-            cell = tf.contrib.rnn.MultiRNNCell(
-                [attn_cell() for _ in range(config.mun_layers)],
-                state_is_tuple=True
-            )
+        def lstm_cell():
+                return tf.contrib.rnn.BasicLSTMCell(size, fotget_biase=0.0, state_is_tuple=True)
+            attn_cell = lstm_cell
+            if is_training and config.keep <1:
+                def attn_cell():
+                    return tf.contrib.rnn.DropoutWrapper(lstm_cell(),output_keep_prob=config.keep_prob)
+                cell = tf.contrib.rnn.MultiRNNCell(
+                    [attn_cell() for _ in range(config.mun_layers)],
+                    state_is_tuple=True
+                )
+                
+            self._initial_state = cell.zero_state(batch_size, tf.float32)
             
-        self._initial_state = cell.zero_state(batch_size, tf.float32)
-        
-        with tf.device("/cpu:0"):
-            embedding = tf.get_variable(
-                "embedding", [vocab_size, size], dtype=data_type())
-                inputs = tf.nn.embedding_lookup(embedding, input_.input_data)
+            with tf.device("/cpu:0"):
+                embedding = tf.get_variable(
+                    "embedding", [vocab_size, size], dtype=data_type())
+                    inputs = tf.nn.embedding_lookup(embedding, input_.input_data)
 
-        if is_training and config.keep_prob < 1:
-            inputs = tf.nn.dropout(inputs, config.keep_prob)
-        outputs = []
-        state = self._initial_state
-        with tf.variable_scope("RNN"):
-            for time_step in range( num_steps ):
-                if time_step > 0 : tf.get_variable_scope().reuse_variables()
-                (cell_output, state) = cell(inputs[:,time_step,:], state)
-                outputs.append(cell_output)
-        output = tf.reshape(tf.constant(outputs, 1),[-1, size])
-        softmax_w = tf.get_variable("softmax_w",[size, vocab_size], dtype=tf.float32)
-        softmax_b = tf.get_variable("softmax_b",[vocab_size], dtype=tf.float32)
-        logits = tf.matmul(output, softmax_w)+ softmax_b
-        loss = tf.contrib.seq2seq.sequence_loss(
-            [logits],
-            [tf.reshape( input_.targets,[-1])],
-            tf.ones([self.batch_size, self.num_steps], dtype=data_type()),
-            average_across_timesteps=False,
-            average_across_batch=True)
+            if is_training and config.keep_prob < 1:
+                inputs = tf.nn.dropout(inputs, config.keep_prob)
+            outputs = []
+            state = self._initial_state
+            with tf.variable_scope("RNN"):
+                for time_step in range( num_steps ):
+                    if time_step > 0 : tf.get_variable_scope().reuse_variables()
+                    (cell_output, state) = cell(inputs[:,time_step,:], state)
+                    outputs.append(cell_output)
+            output = tf.reshape(tf.constant(outputs, 1),[-1, size])
+            softmax_w = tf.get_variable("softmax_w",[size, vocab_size], dtype=tf.float32)
+            softmax_b = tf.get_variable("softmax_b",[vocab_size], dtype=tf.float32)
+            logits = tf.matmul(output, softmax_w)+ softmax_b
+            loss = tf.contrib.seq2seq.sequence_loss(
+                [logits],
+                [tf.reshape( input_.targets,[-1])],
+                [tf.ones([self.batch_size, self.num_steps], dtype=data_type())],
+                )
 
-        # Update the cost
-        self._cost = tf.reduce_sum(loss)
-        self._final_state = state
+            # Update the cost
+            self._cost = tf.reduce_sum(loss) /batch_size
+            self._final_state = state
 
-        if not is_training:
-        return
+            if not is_training:
+            return
 
 neverused = tf.placeholder(dtype= tf.int32, shape=[])
